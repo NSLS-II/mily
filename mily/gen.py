a6f06965ef843f58d3d35b26f9a7df84ac7f0d2f
Thomas A Caswell <tcaswell@bnl.gov>
2018-07-17 20:37:36 -0400

    REF: refactor the generator a bit
    This is starting to look at how this will be extendable.  Do in two
    steps:
    
     1) sort out what input widgets we need
     2) lay those widgets out

--- mily/gen.py
+++ mily/gen.py
@@ -1,9 +1,33 @@
 from qtpy import QtWidgets
 from mily.widgets import MText, MISpin, MFSpin
 import numpy as np
+import inspect
 
 
 class FunctionUI(QtWidgets.QWidget):
+
+    @staticmethod
+    def process_signature(sig):
+        out = {}
+        for k, v in sig.parameters.items():
+            npdt = np.dtype(v.annotation).kind
+            if npdt == 'i':
+                w = MISpin(name=k)
+            elif npdt == 'f':
+                w = MFSpin(name=k)
+                w.setDecimals(3)
+            elif npdt == 'U':
+                w = MText(name=k)
+            else:
+                w = QtWidgets.QLabel(npdt)
+            try:
+                w.setKeyboardTracking(False)
+            except AttributeError:
+                pass
+
+            out[k] = w
+        return out
+
     def __init__(self, func):
         super().__init__()
         self._func = func
@@ -12,7 +36,8 @@ class FunctionUI(QtWidgets.QWidget):
 
         self._params = {}
 
-        for k, v in func.__annotations__.items():
+        for k, w in self.process_signature(
+                inspect.signature(func)).items():
             hlayout = QtWidgets.QHBoxLayout()
             layout.addLayout(hlayout)
 
@@ -20,20 +45,6 @@ class FunctionUI(QtWidgets.QWidget):
             hlayout.addWidget(label)
 
             hlayout.addStretch()
-            npdt = np.dtype(v).kind
-            if npdt == 'i':
-                w = MISpin(name=k, parent=self)
-            elif npdt == 'f':
-                w = MFSpin(name=k, parent=self)
-                w.setDecimals(3)
-            elif npdt == 'U':
-                w = MText(name=k, parent=self)
-            else:
-                w = QtWidgets.QLabel(npdt)
-            try:
-                w.setKeyboardTracking(False)
-            except AttributeError:
-                pass
 
             self._params[k] = w
             hlayout.addWidget(w)
