7b5003167cb3708983ff617f58ee027be50097fb
Andrew Walter <awalter@bnl.gov>
2019-08-15 07:31:18 -0400

    Remove bluesky specific example

--- mily/table_interface_examples.py
+++ mily/table_interface_examples.py
@@ -1,10 +1,5 @@
 from .table_interface import MTableInterfaceWidgetWithExport
 from .widgets import MText, MComboBox, MISpin, MFSpin
-from bluesky.plans import scan
-from bluesky.simulators import summarize_plan
-from ophyd.sim import hw
-
-hw = hw()
 
 
 def partialclass(cls, partial_kwargs):
@@ -72,58 +67,3 @@ class SimpleFunctionWidget(MTableInterfaceWidgetWithExport):
                            'float_var': MFSpin,
                            'int_var': MISpin}
         super().__init__(simple_function, name, *args, **kwargs)
-
-
-# simplest possible tableinterface RunEngine GUI example
-def simple_REfunction(label='label', det=hw.det, motor=hw.motor, start=0,
-                      stop=3, num_steps=3):
-    '''A simple function that can performs summarize_plan on a scan plan.'''
-
-    plan = scan([det], motor, start, stop, num_steps, md={'plan_label': label})
-    summarize_plan(plan)
-
-
-class SimpleREfunctionWidget(MTableInterfaceWidgetWithExport):
-    '''A ``MTableInterfaceWidgetWithExport`` for use with simple_REfunction.
-
-    Adds custom ``detectors``, ``motors``, ``editor_map`` and ``default_rows``
-    attributes which contain some function specfic information and/or user
-    configurable information.
-
-   To run this example as a standalone window use the following:
-
-    ..code-block:: python
-
-        from bluesky_ui.table_interface_examples import SimpleREfunctionWidget
-        from PyQt5.QtWidgets import QApplication
-        app = QApplication([])
-        window = SimpleREfunctionWidget('simple_REfunction')
-        window.show()
-        app.exec_()
-
-    '''
-
-    # NOTE I have made these 'class' variables as I am considering
-    # that we may want to make them traitlets for user config reasons.
-    detectors = [hw.det, hw.det1, hw.det2]
-    motors = [hw.motor, hw.motor1, hw.motor2]
-    default_rows = [
-        {'label': 'plan_1', 'det': hw.det, 'motor': hw.motor, 'start': 0,
-         'stop': 2, 'num_steps': 3},
-        {'label': 'plan_2', 'det': hw.det1, 'motor': hw.motor1, 'start': -3,
-         'stop': 3, 'num_steps': 7},
-        {'label': 'plan_3', 'det': hw.det2, 'motor': hw.motor2, 'start': -4,
-         'stop': 4, 'num_steps': 9}]
-
-    def __init__(self, name, *args, **kwargs):
-        _det_dict = {det.name: det for det in self.detectors}
-        _motor_dict = {motor.name: motor for motor in self.motors}
-        self.editor_map = {'label': MText,
-                           'det': partialclass(MComboBox,
-                                               {'items': _det_dict}),
-                           'motor': partialclass(MComboBox,
-                                                 {'items': _motor_dict}),
-                           'start': MFSpin,
-                           'stop': MFSpin,
-                           'num_steps': MISpin}
-        super().__init__(simple_REfunction, name, *args, **kwargs)
