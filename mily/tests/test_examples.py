020553aa4c0ccdb21efe8fab236985873a6ae57c
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2020-07-11 12:33:08 -0400

    CI: configure travis

--- mily/tests/test_examples.py
+++ mily/tests/test_examples.py
@@ -1,3 +1,13 @@
+from qtpy.QtWidgets import QWidget
+
+
 def test_one_plus_one_is_two():
     "Check that one and one are indeed two."
     assert 1 + 1 == 2
+
+
+def test_gui(qtbot):
+    """Trivial PyQt test"""
+    wd = QWidget()
+    qtbot.addWidget(wd)
+    wd.show()
