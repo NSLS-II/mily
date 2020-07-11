698c7d17dec458545086e74a0512b95e0fcdb865
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2020-07-11 16:42:43 -0400

    TST: test if window manager is working

--- mily/tests/test_examples.py
+++ mily/tests/test_examples.py
@@ -1,4 +1,4 @@
-from qtpy.QtWidgets import QWidget
+from qtpy.QtWidgets import QWidget, QLineEdit
 
 
 def test_one_plus_one_is_two():
@@ -11,3 +11,17 @@ def test_gui(qtbot):
     wd = QWidget()
     qtbot.addWidget(wd)
     wd.show()
+
+
+def test_focus(qtbot):
+    """Check that window manager is working"""
+    line_edit = QLineEdit()
+    qtbot.addWidget(line_edit)
+    with qtbot.waitExposed(line_edit):
+        line_edit.show()
+
+    line_edit.setFocus()
+
+    qtbot.waitUntil(lambda: line_edit.hasFocus())
+
+    assert line_edit.hasFocus()
