ab9834829f6e02cbaf75f0bf3f2eeb1f2953bd7b
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2020-07-13 10:52:29 -0400

    TST: updated the code for loading modules

--- mily/tests/test_imports.py
+++ mily/tests/test_imports.py
@@ -1,28 +1,32 @@
+from importlib import import_module
 import pytest
 
 
-@pytest.mark.parametrize("widget", [
+@pytest.mark.parametrize("name", [
     "MTableItemDelegate", "MTableInterfaceView", "MTableInterfaceWidget",
     "MFunctionTableInterfaceWidget",
     "MText", "MISpin", "MFSpin", "MComboBox", "MCheckBox", "MSelector", "MDateTime",
     "MetaDataEntry", "vstacked_label", "hstacked_label",
 ])
-def test_import_widgets(widget):
-    code = f"from mily.widgets import {widget}"
-    exec(code)
+def test_import_widgets(name):
+    module_name = "mily.widgets"
+    mod = import_module(module_name)
+    assert hasattr(mod, name), f"Error occurred while loading '{name}' from the module '{module_name}'"
 
 
-@pytest.mark.parametrize("widget", [
+@pytest.mark.parametrize("name", [
     "create_qApp",
 ])
-def test_import_core(widget):
-    code = f"from mily.core import {widget}"
-    exec(code)
+def test_import_core(name):
+    module_name = "mily.core"
+    mod = import_module(module_name)
+    assert hasattr(mod, name), f"Error occurred while loading '{name}' from the module '{module_name}'"
 
 
-@pytest.mark.parametrize("widget", [
+@pytest.mark.parametrize("name", [
     "raise_to_operator",
 ])
-def test_import_utils(widget):
-    code = f"from mily.utils import {widget}"
-    exec(code)
+def test_import_utils(name):
+    module_name = "mily.utils"
+    mod = import_module(module_name)
+    assert hasattr(mod, name), f"Error occurred while loading '{name}' from the module '{module_name}'"
