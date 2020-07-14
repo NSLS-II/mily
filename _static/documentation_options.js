1beb13a1d8c55a22e2535b6c9b6548f524e56e18
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-14 14:24:47 +0000

    Deploy NSLS-II/mily to github.com/NSLS-II/mily.git:gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.0.1rc1.post93+g6434cd3',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
